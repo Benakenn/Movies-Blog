8982547ee23e3c2f19a887bc1075bb56ffc875a5
Lan Bui <lantbui27@gmail.com>
2021-09-08 13:58:00 -0700

    changes

---
+++ routes/movieRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const { movies } = require('../db')
+
+router.get('/movies', (req, res) => {
+  res.json(movies)
+})
+
+router.post('/movies', (res, req) => {
+  movies.push(req.body)
+  res.sendStatus(200)
+})
+
+router.put('/movies/:text', (req, res) => {
+  const text = req.params.text
+  movies.forEach(movie => {
+    if (movie.text === text) {
+      item.isWatched = !item.isWatched
+    }
+  })
+  res.sendStatus(200)
+})
+
+router.delete('/items/:text', (req, res) => {
+  const text = req.params.text
+})
